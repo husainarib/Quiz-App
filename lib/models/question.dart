8c96f724eb170c89d8c0982102d25ef8c1c04e1d
husainarib <husainarib@gmail.com>
2024-11-30 16:23:35 -0500

    add decoding for html characters for questions

--- lib/models/question.dart
+++ lib/models/question.dart
@@ -1,5 +1,7 @@
+import 'package:html/parser.dart' show parse;
+
 class Question {
-  final String questionText;
+  String questionText;
   final String correctAnswer;
   final List<String> incorrectAnswers;
   late final List<String> shuffledAnswers;
@@ -9,16 +11,35 @@ class Question {
     required this.correctAnswer,
     required this.incorrectAnswers,
   }) {
+    // Decode HTML entities
+    final document = parse(questionText);
+    final decodedText = document.body?.text ?? questionText;
+
     shuffledAnswers = List<String>.from(incorrectAnswers)
       ..add(correctAnswer)
       ..shuffle();
+
+    questionText = decodedText;
   }
 
   factory Question.fromJson(Map<String, dynamic> json) {
+    final document = parse(json['question']);
+    final decodedQuestionText = document.body?.text ?? json['question'];
+
+    final correctAnswerDocument = parse(json['correct_answer']);
+    final decodedCorrectAnswer =
+        correctAnswerDocument.body?.text ?? json['correct_answer'];
+
+    final incorrectAnswers =
+        List<String>.from(json['incorrect_answers'].map((answer) {
+      final answerDocument = parse(answer);
+      return answerDocument.body?.text ?? answer;
+    }));
+
     return Question(
-      questionText: json['question'] as String,
-      correctAnswer: json['correct_answer'] as String,
-      incorrectAnswers: List<String>.from(json['incorrect_answers']),
+      questionText: decodedQuestionText,
+      correctAnswer: decodedCorrectAnswer,
+      incorrectAnswers: incorrectAnswers,
     );
   }
 }
